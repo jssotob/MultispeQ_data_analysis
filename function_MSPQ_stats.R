2babb41f4d92397a7ee14e3c3b91f46e8c792b99
jssotob <jssotob@gmail.com>
2020-03-19 11:04:42 -0500

    Uploading main functions
    Cleaning and Stats

---
+++ function_MSPQ_stats.R
@@ -0,0 +1,339 @@
+out <- mspq_clean
+
+
+MSPQ_stats <- function(out){ #### function arguments
+
+library(pacman)
+p_load(kmlShape, lmerTest, lme4)
+    
+df_a <- out[["numeric_dataset"]]
+
+df_a$Leaf.Temperature.Differential %<>% abs
+
+SoV <- c("date", "time", out$Sources_ov_Variation)
+
+arr_SoV <- SoV[-c(grep("date", SoV),grep("time", SoV),grep("Gen|gen", SoV))]
+
+means <- df_a %>%              #Outputs 
+  as_tibble %>%                #Outputs 
+  group_by(.dots = SoV) %>%    #Outputs 
+  summarise_all("mean") %>%    #Outputs 
+  ungroup                      #Outputs 
+                               #Outputs 
+st_d <- df_a %>%               #Outputs 
+  as_tibble %>%                #Outputs 
+  group_by(.dots = SoV) %>%    #Outputs 
+  summarise_all("sd") %>%      #Outputs 
+  ungroup                      #Outputs 
+
+
+# ANOVAs ------------------------------------------------------------------
+resp <- c("Leaf.Temperature.Differential", "Relative.Chlorophyll", "phi_index","Phi2", "NPQt", "LEF")
+
+formulas <- list()
+# for(i in 1:length(resp)){
+#   formulas[[i]] <- as.formula(paste0(resp[i],"~ 1+",paste0("as.factor(",SoV[3:length(SoV)],collapse = ")*") ,")*Light.Intensity..PAR.*Ambient.Humidity+as.factor(date)+as.factor(time)"))
+#   names(formulas)[[i]] <- resp[i]
+# }
+
+for(i in 1:length(resp)){
+if(any(grepl("Treat|Trat|treat|trat", SoV))){
+  
+  formulas[[i]] <- as.formula(paste0(resp[i],"~", paste0("as.factor(", c(arr_SoV),")", collapse = "+"), 
+       "+as.factor(time)+(1|",SoV[grep("Gen|gen", SoV)], ")",
+       "+(1|",paste0(SoV[grep("Gen|gen", SoV)]),":",SoV[grep("Treat|Trat|treat|trat", SoV)],")",
+       "+(", paste0("date|", SoV[grep("Gen|gen", SoV)]), ")"))
+  
+} else if (!is.empty(arr_SoV)){
+  
+  formulas[[i]] <- as.formula(paste0(resp[i],"~", paste0("as.factor(", c(arr_SoV),")", collapse = "+"), 
+         "+as.factor(time)+(1|",SoV[grep("Gen|gen", SoV)],")",
+         "+(", paste0("date|", SoV[grep("Gen|gen", SoV)]), ")"))
+  
+} else {
+  formulas[[i]] <- as.formula(paste0(resp[i],"~",  
+                                     "as.factor(time)+(1|",SoV[grep("Gen|gen", SoV)],")",
+                                     "+(", paste0("date|", SoV[grep("Gen|gen", SoV)]), ")"))
+  }
+}
+rm(i)
+
+
+ANOVAs <- list() #Outputs
+
+for(i in 1:length(resp)){
+  ANOVAs[[i]] <- lmer(formulas[[i]], data = means, control = lmerControl(optimizer ="Nelder_Mead")) %>% summary
+  names(ANOVAs)[[i]] <- resp[i]
+}
+
+rm(formulas, resp, i)
+
+# Ranks -------------------------------------------------------------------
+
+dates_rank <- lapply(as.character(unique(means$date)), function(x)filter(means, date == x))
+
+dates_rank <- lapply(1:length(dates_rank), function(x){
+  as.data.frame(dates_rank[[x]][,-which(names(dates_rank[[x]])=="time")])
+  })
+
+for(i in 1:length(dates_rank)){
+dates_rank[[i]] %<>% 
+  group_by(.dots = SoV[3:length(SoV)]) %>% 
+  summarise_all("mean") %>% 
+  ungroup %>% 
+  as.data.frame
+}
+rm(i)
+
+x <- c("Leaf.Temperature.Differential", "Relative.Chlorophyll", "phi_index","Phi2", "LEF", "Thickness")
+ranks <- list()
+
+if(is.empty(arr_SoV)){
+  
+  for(i in 1:length(dates_rank)){
+    per_date <- list()
+    
+    for(j in 1:length(x)){
+      per_date[[j]] <- dates_rank[[i]][order(-dates_rank[[i]][,x[j]]),] %>% 
+        droplevels %>% 
+        as_tibble %>% 
+        dplyr::select(contains(c(SoV[grep("Gen|gen", SoV)]))) %>% 
+        dplyr::mutate(x = 1:dplyr::n()) %>% 
+        arrange_(.dots = SoV[grep("Gen|gen", SoV)]) %>% 
+        as.data.frame
+      names(per_date)[[j]] <- x[j]
+    }
+  
+  
+  scores <- paste0(x,"_score")
+  ranks[[i]] <- join_all(per_date, by= SoV[3:length(SoV)], type='left')
+  names(ranks[[i]])[grep("x", names(ranks[[i]]))] <- scores
+  ranks[[i]]$final_score <- rowSums(ranks[[i]][,grep("_score", names(ranks[[i]]))])
+  ranks[[i]]$date <- dates_rank[[i]]$date[1]
+  }
+  
+} else{
+  
+for(i in 1:length(dates_rank)){
+  per_date <- list()
+  
+  for(j in 1:length(x)){
+    per_date[[j]] <- dates_rank[[i]][order(dates_rank[[i]][,c(arr_SoV)], -dates_rank[[i]][,x[j]]),] %>% 
+      droplevels %>% 
+      as_tibble %>% 
+      dplyr::select(contains(c(SoV[grep("Gen|gen", SoV)], arr_SoV))) %>% 
+      group_by(.dots = arr_SoV) %>%
+      dplyr::mutate(x = 1:dplyr::n()) %>% 
+      arrange_(.dots = SoV[grep("Gen|gen", SoV)]) %>% 
+      as.data.frame
+      names(per_date)[[j]] <- x[j]
+  }
+
+  
+  scores <- paste0(x,"_score")
+  ranks[[i]] <- join_all(per_date, by= SoV[3:length(SoV)], type='left')
+  names(ranks[[i]])[grep("x", names(ranks[[i]]))] <- scores
+  ranks[[i]]$final_score <- rowSums(ranks[[i]][,grep("_score", names(ranks[[i]]))])
+  ranks[[i]]$date <- dates_rank[[i]]$date[1]
+}  
+  }
+rm(per_date)
+
+
+# Rank Selection ----------------------------------------------------------
+
+gen_le <- means[,grep("Gen|gen", SoV)] %>% as.data.frame() %>% .[,1] %>% unique %>% length
+
+gen_le_5 <- round(gen_le*0.05)
+
+if(gen_le_5==0){
+  gen_le_5 <- 1
+}
+
+if(is.empty(arr_SoV)){
+  
+  selection <- list()
+  
+  for(s in 1:length(ranks)){
+    good <- ranks[[s]] %>%
+      arrange(final_score) %>% 
+      .[,grep("Gen|gen", names(ranks[[s]]))] %>% head(.,n=gen_le_5) %>% as.character
+    
+    bad <- ranks[[s]] %>%
+      arrange(final_score) %>% 
+      .[,grep("Gen|gen", names(ranks[[s]]))] %>% tail(.,n=gen_le_5) %>% as.character
+    
+    selection[[s]] <- data.frame(date = ranks[[s]]$date[1:gen_le_5],
+                                 select_good = good,
+                                 select_bad = bad)
+    rm(good,bad)
+  }
+} else if(length(arr_SoV)==1){
+  if(length(unique(ranks[[1]][,arr_SoV]))==1){
+    
+    selection <- list()
+    
+    for(s in 1:length(ranks)){
+    good <- ranks[[s]] %>%
+    arrange(final_score) %>% 
+    .[,grep("Gen|gen", names(ranks[[s]]))] %>% head(.,n=gen_le_5) %>% as.character
+    
+    bad <- ranks[[s]] %>%
+      arrange(final_score) %>% 
+      .[,grep("Gen|gen", names(ranks[[s]]))] %>% tail(.,n=gen_le_5) %>% as.character
+    
+    selection[[s]] <- data.frame(date = ranks[[s]]$date[1:gen_le_5], SoV = arr_SoV, 
+                                 SoV_ID = unique(ranks[[s]][,arr_SoV]),
+                                 select_good = good,
+                                 select_bad = bad)
+    rm(good,bad)
+    }
+  } else {
+    
+    selection <- list()
+    
+    
+    for(m in 1:length(ranks)){
+      a <- list()
+    sel <- lapply(unique(ranks[[m]][,arr_SoV]), function(x) subset(ranks[[m]], ranks[[m]][,arr_SoV]==x))
+    
+    for(i in 1:length(sel)){
+  
+        good <- sel[[i]] %>%
+        arrange(final_score) %>% 
+        .[,grep("Gen|gen", names(sel[[i]]))] %>% head(.,n=gen_le_5) %>% as.character
+      
+      bad <- sel[[i]] %>%
+        arrange(final_score) %>% 
+        .[,grep("Gen|gen", names(sel[[i]]))] %>% tail(.,n=gen_le_5) %>% as.character
+      
+      a[[i]] <- data.frame(select_good = good,
+                              select_bad = bad,
+                              date = unique(sel[[i]]$date),
+                              SoV = arr_SoV,
+                              SoV_ID = unique(sel[[i]][,arr_SoV]))
+      rm(good,bad)
+    }
+    selection[[m]] <- do.call(rbind, a)
+    }
+    rm(a,sel)
+  }
+} else{
+
+  #### mas de una fuente de variacion diferente a genotipo??  
+}
+
+
+date_len <- length(dates_rank)
+
+ranks <- do.call(rbind, ranks) #Outputs
+
+ranks$date %<>% ymd
+
+selection <- do.call(rbind, selection) #Outputs
+
+sel_good <- selection$select_good %>% table %>% as.data.frame() %>% arrange(desc(Freq))
+sel_bad <- selection$select_bad %>% table %>% as.data.frame() %>% arrange(desc(Freq))
+
+names(sel_good)[which(names(sel_good)==".")] <- SoV[grep("Gen|gen", SoV)]
+names(sel_bad)[which(names(sel_bad)==".")] <- SoV[grep("Gen|gen", SoV)]
+
+sel_bad %<>% 
+  mutate(Freq_rel = Freq/date_len)
+
+sel_good %<>% 
+  mutate(Freq_rel = Freq/date_len)
+
+ranking <- list(number_of_samplings = date_len, 
+                scores = ranks,
+                selection_per_date = selection,
+                Freq_good = sel_good,
+                Freq_bad = sel_bad)      ###### Outputs
+
+output <- list(means = means,
+               std = st_d,
+               ANOVA = ANOVAs,
+               ranking = ranking)
+
+return(output)
+}
+
+# cor_means <- cor(means[-c(1:4)])
+# corrplot(cor_means, method = "number", type = "upper", tl.cex = 0.5, number.cex = 0.5)
+# 
+# times <- means$date %>% unique %>% length
+#   
+# AM <- means %>% filter(time == "AM") %>% arrange(Genotipo)
+# 
+# ######IDENTIFICAR Y COMPLETAR n PARTIENDO POR GENOTIPOS Y VALUE NA
+# AM %>% 
+#   group_by(Genotipo) %>% 
+#   summarise(n = n()) %>% arrange(n) %>% View
+# 
+# 
+# 
+# 
+# 
+# AM_clds <- AM[,c("Genotipo", "Leaf.Temperature.Differential")] %>%
+#   mutate(Genotipo = as.factor(Genotipo)) %>% as.data.frame
+# 
+# 
+# gens <- lapply(unique(AM_clds$Genotipo), function(x)filter(AM_clds,Genotipo == x))
+# 
+# for(i in 1:length(gens)){
+# gens[[i]]$times <- 0:(nrow(gens[[i]])-1)/10
+# gens[[i]] %<>% dplyr::select(Genotipo, times, Leaf.Temperature.Differential)
+# }
+# 
+# AM_clds <- do.call(rbind,gens)
+# rm(gens)
+# 
+# set.seed(1)
+# 
+# nored <- AM_clds %>% as.data.frame %>% 
+#   cldsLong %>% kmlShape(., toPlot = "none")
+# 
+# ###plotting K-means
+# nored@trajMeans %>% 
+#   ggplot(.,aes(x = times, y = traj, colour = factor(iCenters)))+
+#   geom_line(size = 1)+
+#   ylim(c(-10,2))
+# 
+# 
+# 
+# 
+# df_a %>% names
+# 
+# mod <- df_a %>% 
+#   as_tibble %>% 
+#   dplyr::select(date,time, Genotipo,R,G,B,Phi2,PhiNO,PhiNPQ,phi_index, Thickness, LEF, Relative.Chlorophyll,
+#                 Leaf.Temperature.Differential) %>% 
+#   mutate(Genotipo = as.factor(Genotipo)) %>%
+#   #group_by(date,time,Genotipo,Tratamiento) %>% 
+#   #summarise_all("mean") %>% 
+#   #ungroup %>% 
+#   mutate(date = as.factor(date)) %>% 
+#   aov(Phi2 ~ time + Genotipo + Error(date/Genotipo), data = .)
+# 
+# summary(mod)
+# 
+# dates <- list()
+# 
+# 
+# fechas <- df_a$date %>% unique %>% as.character %>% sort
+# for(i in 1:length(fechas)){
+#   dates[[i]] <- filter(df_a, date == fechas[i])
+#   names(dates)[[i]] <- fechas[i]
+# }
+# rm(i)
+# 
+# dates[[1]] %>% str
+# 
+# ?cldsLong
+# 
+# 
+# g <- function(x)dnorm(0:100,runif(1,25,75),10)*rnorm(1,5,1)
+# dn <- data.frame(id=rep(1:200,each=101),
+#                  times=rep((0:100)/10,times=20),
+#                  traj=as.numeric(sapply(1:200,g)))
+# 
---
+++ function_MSPQ_tidy.R
@@ -0,0 +1,266 @@
+library(pacman)
+p_load(plotly, robustbase, TestDataImputation, dplyr, ggplot2, imputeTS, installr, corrplot, stringr, magrittr, ez, lubridate, tidyverse)
+
+
+MSPQ_tidy <- function(df, data_name){
+  #-----delete Plot.1 column (repeated)-----
+  r_full <- nrow(df)
+  c_full <- ncol(df)
+  delete <- which(names(df) == "Plot.1")
+  if(!is.empty(delete)){df <- df[,-delete]}
+  rm(delete)
+  #-----Replacing null for NA-----
+  cat("Replacing null for NA\n")
+  
+  for(i in 1:ncol(df)){
+    if(any(df[,i]=="null", na.rm = T)){
+      df[,i] <- as.numeric(gsub(x = df[,i], pattern = "null", replacement = NA))
+    }
+  }
+  rm(i)
+  #-----------------Calculating Phi index--------------------------------
+  cat("Calculating Phi Index\n")
+  
+  df %<>% 
+    mutate(phi_index = Phi2/(PhiNPQ+PhiNO))
+  
+  
+  #-----Formating dates and creating time (morning/afternoon) column-----
+  cat("Formating dates and creating time (morning/afternoon) column\n")
+  
+  df$time %<>%  as.character
+  
+  if(grepl(x = df$time[1], pattern = ":.*AM|PM&AM|PM")){
+    
+    x <- list()
+    
+    for(i in 1:length(df$time)){
+      
+      x[[i]] <- unlist(strsplit(df$time[i], " "))[-2]
+      x[[i]] <- paste0(x[[i]][1]," ", x[[i]][2])
+    }
+    
+    x <- unlist(x)
+    df$time <- x
+    rm(x,i)
+    
+    names(df)[names(df) == "time"] <- "date"
+    
+    df <- separate(df,date,into = c("date", "time"), sep = " ")
+    
+    df$date <- as.Date(df$date, format = "%m/%d/%Y")
+    df$time <- as.factor(df$time)
+  } else {
+    stop("there is not AM/PM indicator in column time and/or hour is missing, check out first\n")
+  }
+  
+  #-----Discarding rows with issues-----
+  
+  cat("Discarding rows with issues\n")
+  
+  issues_index <- which(!is.na(df$Issues))
+  r_issue <- length(issues_index)
+  if(r_issue==0){issues_index <- c()}
+  
+  #-----Subsetting factors and character columns out from df (NOT to be included into final df)-----
+  
+  cat("Subsetting factors and character columns out from df (NOT to be included into final df)\n") 
+  factor_index <-c() 
+  
+  for(i in 1:ncol(df)){
+    if(is.factor(df[,i])){
+      factor_index[i] <- i
+    }else if(is.character(df[,i])){
+      factor_index[i] <- i
+    }else if(length(which(is.na(df[,i]))) == nrow(df) || length(unique(df[,i])) == 1 ||length(unique(df[,i])) == 2){
+      factor_index[i] <- i  
+    }else if(length(unique(df[,i])) == 3 || length(which(is.na(df[,i]))) >= round(nrow(df)*0.5,digits = 0)){
+      factor_index[i] <- i
+    } else if (is.Date(df[,i])){
+      factor_index[i] <- i
+    }
+    
+  }
+  rm(i)
+  factor_index <- factor_index[!is.na(factor_index)]
+  factor_index <- c(factor_index,which(names(df)=="ID"))
+  #-----Applying control structures-----
+  cat("Applying control structures\n")
+  
+  SoV_index <- which(names(df)=="date"): (which(names(df)=="absorbance_420")-1)
+  factor_index <- factor_index[!factor_index %in% SoV_index]
+  SoV_names <- names(df)[SoV_index[3:length(SoV_index)]]
+  
+  
+  num_df <- df[,-factor_index]
+  factor_df <- df[,factor_index]
+  c_factor <- length(factor_index)
+  rm(factor_index)
+  
+  #----finding absorbances with NA's (NOT to be included into final df)----
+  
+  absorbance_index <- which(grepl(x = names(num_df), pattern = "absorbance_"))
+  absorbances <- unlist(strsplit(names(num_df)[absorbance_index], "absorbance_"))
+  absorbances <- absorbances[-which(absorbances=="")]
+  
+  absorbance<- c()
+  for(i in 1:length(absorbance_index)){
+    if(length(which(is.na(num_df[,paste0("absorbance_",absorbances[i])])))!=0){
+      absorbance[i] <- absorbances[i]
+    }
+  }
+  rm(i)
+  
+  absorbance <-absorbance[!is.na(absorbance)]
+  
+  absorbance_index <- c() 
+  
+  for(i in 1:length(absorbance)){
+    absorbance[i] <- paste0("absorbance_",absorbance[i])
+    absorbance_index[i] <- which(names(num_df)==absorbance[i])
+  }
+  rm(i);rm(absorbance);rm(absorbances)
+  c_absorbance <- length(absorbance_index)
+  
+  if(is.empty(absorbance_index)){
+    absorbance_index <- NA
+  }
+  
+  #----Comparing Rel_clo with SPAD_605, if identical (NOT to be included into final df)----
+  
+  if(identical(df$Relative.Chlorophyll,df$SPAD_650)){
+    clo_index <- as.numeric(which(names(num_df)=="SPAD_650"))
+    clo_rem <- 1
+  } else {
+    clo_index <- NA
+    clo_rem <- 0
+  }
+  
+  #----if NA's in spatial coordinates, removing the columns (NOT to be included into final df)----
+  if(any(names(num_df) == "Latitude")){
+    if(any(is.na(num_df$Latitude))){
+      location_index <- c(which(names(num_df)=="Latitude"), which(names(num_df)=="Longitude"))
+      c_lon_lat_a <- 2
+      c_lon_lat <- 2
+    } else {
+      location_index <- NULL
+      c_lon_lat <- 0
+      c_lon_lat_a <- 0
+    }
+  } else {
+    location_index <- NULL
+    c_lon_lat <- 2
+    c_lon_lat_a <- 0
+  }
+  
+  f_indices <- unique(c(absorbance_index, clo_index,location_index))
+  f_indices <- f_indices[!is.na(f_indices)]   
+  
+  if(!is.empty(f_indices)){
+    factor_df <- cbind(factor_df,num_df[,f_indices])
+    num_df <- num_df[,-f_indices]
+  }
+  
+  rm(absorbance_index,clo_index, f_indices)
+  
+  #-----Finding and removing rows with NA's-----
+  cat("Finding and removing rows with NA's\n")
+  row_NA <- list()
+  
+  
+  for(i in 1:ncol(num_df)){
+    row_NA[[i]] <- num_df[,i] %>% 
+      is.na() %>% 
+      which() 
+  }
+  rm(i)
+  
+  row_NA <- unique(unlist(row_NA))
+  r_rem <- length(row_NA)
+  #-----Discarding rows with PAR Phi2, PhiNPQ, PhiNO and Rel_clo out of range-----
+  cat("Discarding rows with PAR Phi2, PhiNPQ, PhiNO and Rel_clo out of range\n")
+  
+  par_out <- which(num_df$Light.Intensity..PAR. > 2500 | num_df$Light.Intensity..PAR. < 1)  
+  phi2_out <- which(num_df$Phi2 > 0.85 | num_df$Phi2 < 0.05)
+  phinpq_out <- which(num_df$PhiNPQ > 0.85 | num_df$PhiNPQ < 0)
+  phino_out <- which(num_df$PhiNO > 0.5 | num_df$PhiNO < 0)
+  
+  if(clo_rem==1){
+    rel_clo_out <- which(num_df$Relative.Chlorophyll > 75 | num_df$Relative.Chlorophyll < 0)
+  } else if(clo_rem==0) {
+    rel_clo_out <- which(num_df$SPAD_650 > 75 | num_df$SPAD_650 < 0)
+  }
+  psac <- which(num_df$PS1.Active.Centers > 50 | num_df$PS1.Active.Centers < -50)
+  psopc <- which(num_df$PS1.Open.Centers > 50 | num_df$PS1.Open.Centers < -50)
+  psorc <- which(num_df$PS1.Over.Reduced.Centers > 50 | num_df$PS1.Over.Reduced.Centers < -50)
+  psoxc <- which(num_df$PS1.Oxidized.Centers > 50 | num_df$PS1.Oxidized.Centers < -50)
+  npqt <- which(num_df$NPQt > 30 | num_df$NPQt < -30)
+  vh <- which(num_df$vH. > 10 | num_df$vH. < -10)
+  ecs_m <- which(num_df$ECSt.mAU > 5 | num_df$ECSt.mAU < -5)
+  gh <- which(num_df$gH. > 1200 | num_df$gH. < -1200)
+  ecs_t <- which(num_df$ECS_tau > 100 | num_df$ECS_tau < -100)
+  
+  rows_out <- unique(c(issues_index, row_NA))
+  out_param <- unique(c(par_out, phi2_out, phinpq_out, phino_out, rel_clo_out, psac, psopc, psorc, psoxc, npqt, vh, ecs_m, gh, ecs_t))
+  final_removals <- unique(c(rows_out, out_param))
+  r_out_param <- length(out_param)
+  
+  #-----Making final df-----
+  cat("Making final df\n")
+  if(!is.empty(final_removals)){
+    num_df <- num_df[-final_removals,]
+    factor_df <- factor_df[-final_removals,]
+    rm_data <- df[final_removals,]
+  } else {
+    rm_data <- NA
+  }
+  
+
+  #-----Making summary table-----
+  cat("Making summary table\n")
+  summ_tab <- as.data.frame(matrix(NA, 1))
+  names(summ_tab) <- "file_name"
+  summ_tab$file_name <- data_name ###### index with list.files()
+  summ_tab$total_obs <- r_full
+  summ_tab$total_variables <- c_full
+  summ_tab$non_numeric_variables_removed <- c_factor
+  summ_tab$absorbance_variables_removed_by_NA <- c_absorbance
+  summ_tab$LAT_LON_removed <- c_lon_lat
+  summ_tab$SPAD_650_removed <- clo_rem
+  summ_tab$obs_removed_by_Issues <- r_issue
+  summ_tab$obs_removed_by_NA <- r_rem
+  summ_tab$obs_removed_by_param_out <- r_out_param
+  summ_tab$Total_obs_removed <- length(final_removals)
+  summ_tab$perc_removed_obs <- round(((length(final_removals))*100/r_full), digits = 2)
+  summ_tab$perc_removed_var <- round(((c_lon_lat_a+clo_rem+c_factor+c_absorbance)*100/c_full), digits = 1)
+  
+  
+  #----Dropping empty levels in factors----
+  num_df <- droplevels(num_df)
+  factor_df <- droplevels(factor_df)
+  if(is.data.frame(rm_data)){
+  rm_data <- droplevels(rm_data)
+  }
+  #----output list----
+  if(summ_tab$perc_removed_obs!=0){
+    rm_table <- rm_data[,SoV_index[-2]] %>% table %>% as.data.frame
+    if(any(rm_table$Freq==0)){
+      rm_table <- rm_table[-which(rm_table[,"Freq"]==0),]
+    }
+    rm_table$date <- ymd(rm_table$date)
+    rm_table %<>% arrange(Freq)
+    
+  } else {
+    rm_table <- "No observations were removed"
+  }
+  summ_tab %<>% t
+  
+  out <- list(num_df, factor_df, summ_tab, rm_data, rm_table, SoV_names)
+  names(out) <- c("numeric_dataset", "non_numeric_dataset", "summary", "removed_observations", "removed_freq", "Sources_ov_Variation")
+  
+  
+  
+  cat("Done!!!\n")
+  return(out)
+}
+
